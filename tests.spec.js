a5f791ac62f3c88aa36906903bad948451b02f77
Sheridan McKisick <sheridan@heysparkbox.com>
2021-03-21 20:32:28 -0400

    Add tests for updating timer message

--- tests.spec.js
+++ tests.spec.js
@@ -2,11 +2,22 @@ import Timer from './src/timer';
 
 describe('Setting the timer', () => {
   const timerSetTime = new Timer();
-  it('Shows the default time of 60 seconds', () => {
+  test('Shows the default time of 60 seconds', () => {
     expect(timerSetTime.remainingTime).toEqual(60);
   });
-  it('Updates the default time to 45 seconds', () => {
+  test('Updates the default time to 45 seconds', () => {
     timerSetTime.setTimer(45);
     expect(timerSetTime.remainingTime).toEqual(45);
+  });
+});
+
+describe('Updating the timer message', () => {
+  const timerSetMessage = new Timer();
+  test('Shows the default message on the timer is "Touch to Begin"', () => {
+    expect(timerSetMessage.message).toBe('Touch to Begin');
+  });
+  test('Updates the timer message to "Pause"', () => {
+    timerSetMessage.updateMessage('Pause');
+    expect(timerSetMessage.message).toBe('Pause');
   });
 });
