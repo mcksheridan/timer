d670053c8b01bfc94ffdf4a79a17f5bca6178a4e
Sheridan McKisick <sheridan@heysparkbox.com>
2021-03-22 08:52:22 -0400

    Add state property to timer and create updateState function

--- src/timer.js
+++ src/timer.js
@@ -3,11 +3,16 @@ class Timer {
     this.startingTime = 60;
     this.timerSpeed = 100;
     this.remainingTime = this.startingTime;
+    this.state = 'Uninitialized';
     this.message = 'Touch to Begin';
   }
 
   setTimer(time) {
     this.remainingTime = time;
+  }
+
+  updateState(state) {
+    this.state = state;
   }
 
   updateMessage(message) {
