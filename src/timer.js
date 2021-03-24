fd5b2df4191d175404d8d63639860e0560941be0
Sheridan McKisick <sheridan@heysparkbox.com>
2021-03-23 22:29:43 -0400

    Combine JavaScript into one file (timer.js) and import that file into timer.html

--- src/timer.js
+++ src/timer.js
@@ -1,5 +1,27 @@
-// eslint-disable-next-line import/extensions
-import template from './timerHTML.js';
+const template = `
+
+  <link rel=stylesheet type=text/css href="timer.css">
+
+    <section>
+
+      <header>
+        <h2><span class="timer-header_seconds"></span> Second Timer</h2>
+      </header>
+
+      <main>
+        <svg class="timer-bar" width="90vw" height="2.5rem">
+          <rect class="timer-bar_background" width="100%" height="40" />
+          <rect class="timer-bar_progress" width="100%" height="40" />
+          <text class="timer-bar_remaining-time" x="1%" y="60%">0</text>
+        </svg>
+      </main>
+
+      <footer class="timer-button">
+          <button type="button" class="timer-button_control">Loading...</button>
+      </footer>
+
+    </section>
+`;
 
 class Timer extends HTMLElement {
   constructor() {
@@ -80,3 +102,69 @@ class Timer extends HTMLElement {
 }
 
 export default Timer;
+
+window.customElements.define('timer-object', Timer);
+
+const timer = new Timer();
+
+const timerButton = document.querySelector('.timer-button_control');
+const timerProgressBar = document.querySelector('.timer-bar_progress');
+const timerRemainingTime = document.querySelector('.timer-bar_remaining-time');
+
+function updateTimerUI() {
+  timerRemainingTime.innerHTML = timer.remainingTime;
+  timerButton.innerHTML = timer.message;
+  timerProgressBar.setAttribute('width', `${timer.progressPercent}%`);
+  // Update timer class
+  if (timer.state === 'Active') {
+    timerButton.classList.add('timer_message--active');
+    timerButton.classList.remove('timer_message--paused');
+  }
+  if (timer.state === 'Paused') {
+    timerButton.classList.remove('timer_message--active');
+    timerButton.classList.add('timer_message--paused');
+  }
+}
+
+function decrementTimer() {
+  timer.decrementTimeRemaining();
+  timer.decrementProgressBar();
+  updateTimerUI();
+}
+
+function initializeTimer() {
+  setInterval(decrementTimer, timer.timerSpeed);
+}
+
+function beginTimer() {
+  timer.updateState('Active');
+  timer.updateMessage('Pause');
+  initializeTimer();
+  updateTimerUI();
+}
+
+function pauseTimer() {
+  timer.updateState('Paused');
+  timer.updateMessage('Resume');
+  updateTimerUI();
+}
+
+function resumeTimer() {
+  timer.updateState('Active');
+  timer.updateMessage('Pause');
+  updateTimerUI();
+}
+
+timerButton.addEventListener('click', () => {
+  if (timer.state === 'Uninitialized') {
+    beginTimer();
+    return;
+  }
+  if (timer.state === 'Active') {
+    pauseTimer();
+    return;
+  }
+  if (timer.state === 'Paused') {
+    resumeTimer();
+  }
+});
