8e216b7667b72d0f49fd8979b0231608a5eeb086
Sheridan McKisick <sheridan@heysparkbox.com>
2021-03-26 21:51:14 -0400

    Remove test where timer returns default time. There is no longer a default time because the starting time of each timer is determined by the time attribute on its custom element. This test in its current form is no longer valuable.

--- tests.spec.js
+++ tests.spec.js
@@ -2,9 +2,6 @@ import Timer from './src/timer';
 
 describe('Setting the timer', () => {
   const timerSetTime = new Timer();
-  test('Shows the default time of 60 seconds', () => {
-    expect(timerSetTime.remainingTime).toEqual(60);
-  });
   test('Updates the default time to 45 seconds', () => {
     timerSetTime.setTimer(45);
     expect(timerSetTime.remainingTime).toEqual(45);
